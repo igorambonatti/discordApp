08655dd812ede025e637296f5f1d75bfcae8642e
Igor Feldman Ambonatti <igor.ambonatti@pampa.compasso>
2020-07-07 15:25:38 -0300

    ServerName

---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container, Title, ExpandIcon } from "./styles";
+
+const ServerName: React.FC = () => {
+  return (
+    <Container>
+      <Title>Ambonatti SERVER</Title>
+
+      <ExpandIcon />
+    </Container>
+  );
+};
+
+export default ServerName;
